c94100058ad5819312c3444a8b1a87948eeb5c11
whybruhh <57416121+whybruhh@users.noreply.github.com>
2022-02-03 20:08:13 -0800

    Update SkillDemoTest.java

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void multiply() {
-        assertEquals(4, SkillDemo.multiply(1, 3));
+        assertEquals(3, SkillDemo.multiply(1, 3));
     }
 }
